ccb29b0a93937f4d661095499ca0589f65617f21
Moises <mhdezcoronado@gmail.com>
2022-04-04 18:17:24 +0200

    Create a reducer and action for items

--- src/features/items/reducer.js
+++ src/features/items/reducer.js
@@ -1,65 +1,26 @@
 /* eslint-disable camelcase */
-import authApi from './api';
+import { fetchItems } from './api';
 
-const SET_USER_REGISTRATION = 'auth/registration/SET_USER_REGISTRATION';
-const SET_USER_SESSION = 'auth/session/SET_USER_SESSION';
-const SET_DESTROY_USER_SESSION = 'auth/session/SET_DESTROY_USER_SESSION';
+const SET_ITEMS = 'item/SET_ITEMS';
+
 const initialState = {
-  name: '',
-  email: '',
-  Authorization: null,
-  success: null,
+  items: [],
 };
 
-const setSingUp = (payload) => ({
-  type: SET_USER_REGISTRATION,
+const setItems = (payload) => ({
+  type: SET_ITEMS,
   payload,
 });
 
-export const setSingUpApi = (endPoint, payload) => async (dispatch) => {
-  const data = await authApi(endPoint, payload);
-  if (data) {
-    dispatch(setSingUp(data));
-  }
-};
-
-const setSingOut = (payload) => ({
-  type: SET_DESTROY_USER_SESSION,
-  payload,
-});
-
-export const setSingOutApi = (endPoint) => async (dispatch) => {
-  authApi(endPoint);
-  dispatch(setSingOut());
-};
-
-const setSingIn = (payload) => ({
-  type: SET_USER_SESSION,
-  payload,
-});
-
-export const setSingInApi = (endPoint, payload) => async (dispatch) => {
-  const data = await authApi(endPoint, payload);
-  if (data) {
-    dispatch(setSingIn(data));
-  }
+export const loadItems = () => async (dispatch) => {
+  const data = await fetchItems();
+  dispatch(setItems(data));
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case SET_USER_REGISTRATION:
-      return action.payload;
-
-    case SET_DESTROY_USER_SESSION:
-      return {
-        name: '',
-        email: '',
-        Authorization: null,
-        success: null,
-      };
-
-    case SET_USER_SESSION:
-      return action.payload;
+    case SET_ITEMS:
+      return { ...state, items: action.payload };
 
     default:
       return state;
