fcae11f6d3087eba3665af2588f5bb44771514be
Moises <mhdezcoronado@gmail.com>
2022-04-04 12:01:48 +0200

    Update authReducer & actions

--- src/features/auth/reducer.js
+++ src/features/auth/reducer.js
@@ -5,36 +5,33 @@ const SET_USER_REGISTRATION = 'auth/registration/SET_USER_REGISTRATION';
 const SET_USER_SESSION = 'auth/session/SET_USER_SESSION';
 const SET_DESTROY_USER_SESSION = 'auth/session/SET_DESTROY_USER_SESSION';
 const initialState = {
-  name: '',
-  email: '',
-  Authorization: null
+  message: [],
+  status: '',
 };
 
-export const setSingUpApi = (endPoint, payload) => async dispatch => {
+export const setSingUpApi = (endPoint, payload) => async () => {
   const data = await authApi(endPoint, payload);
-  return data
+  return data;
 };
 
-const setSingOut = payload => ({
+const setSingOut = (payload) => ({
   type: SET_DESTROY_USER_SESSION,
   payload,
 });
 
-export const setSingOutApi = (endPoint) => async dispatch => {
+export const setSingOutApi = (endPoint) => async (dispatch) => {
   authApi(endPoint);
   dispatch(setSingOut());
-}
+};
 
-const setSingIn = payload => ({
+const setSingIn = (payload) => ({
   type: SET_USER_SESSION,
   payload,
 });
 
-export const setSingInApi = (endPoint, payload) => async dispatch => {
+export const setSingInApi = (endPoint, payload) => async (dispatch) => {
   const data = await authApi(endPoint, payload);
-  if (data) {
-    dispatch(setSingIn());
-  }
+  dispatch(setSingIn(data));
 };
 
 const reducer = (state = initialState, action) => {
@@ -43,18 +40,16 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         success: true,
-      }
+      };
 
     case SET_DESTROY_USER_SESSION:
       return {
-        name: '',
-        email: '',
-        Authorization: null,
-        success: null
-      }
+        message: [],
+        status: '',
+      };
 
     case SET_USER_SESSION:
-      return action.payload
+      return action.payload;
 
     default:
       return state;
