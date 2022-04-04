e9cfa18b887b0b72f9f7a34f423c5fb40cc646f8
Moises <mhdezcoronado@gmail.com>
2022-04-04 18:17:53 +0200

    Create Idex component to display items

--- src/features/items/Index.js
+++ src/features/items/Index.js
@@ -1,3 +1,36 @@
-const Items = () => <h1>Display all the dances</h1>;
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadItems } from './reducer';
+
+const Items = () => {
+  const items = useSelector(({ itemsReducer }) => itemsReducer.items);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!items.length) {
+      dispatch(loadItems());
+    }
+  }, []);
+
+  return (
+    <ul>
+      <h1>Items</h1>
+      {items.map((v) => (
+        <li key={v.id}>
+          <div>
+            <img src={v.picture} alt={v.description} />
+          </div>
+          <p>{v.name}</p>
+          <p>{v.description}</p>
+          <p>{v.finance}</p>
+          <p>{v.option}</p>
+          <p>{v.total}</p>
+          <p>{v.duration}</p>
+          <p>{v.apr}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Items;
