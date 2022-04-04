d89340959b7e49dc53bb3940e22dbbed1606e226
Moises <mhdezcoronado@gmail.com>
2022-04-04 12:02:59 +0200

    Fix linters

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './store';
 import App from './App';
+import store from './store';
 
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
-    </Provider>
+    </Provider>,
   );
 
   expect(getByText(/learn/i)).toBeInTheDocument();
--- src/features/auth/Registration.js
+++ src/features/auth/Registration.js
@@ -1,88 +1,92 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { setSingUpApi } from '../auth/reducer';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { setSingUpApi } from './reducer';
 
 const Registration = () => {
-  const dispatch = useDispatch()
-  let navigate = useNavigate();
-  const [message, setMessage] = useState(['All fields are required'])
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [message, setMessage] = useState(['All fields are required']);
   const [state, setState] = useState(
     {
       name: '',
       email: '',
       password: '',
-      confirmation_password: ''
-    }
-  )
+      confirmation_password: '',
+    },
+  );
 
   const handleInput = (event) => {
-    setState({ ...state, [event.target.name]: event.target.value })
-  }
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
 
   const handleSubmit = (event) => {
-
-    event.preventDefault()
+    event.preventDefault();
     dispatch(setSingUpApi('users', state))
-      .then(response => {
-        setMessage(response.message)
+      .then((response) => {
+        setMessage(response.message);
         if (response.status) {
-          navigate('/', { replace: true })
+          navigate('/sing_in', { replace: true });
         }
       })
-      .catch(e => {
+      .catch((e) => {
+        // eslint-disable-next-line no-console
         console.log(e);
-      })
-  }
+      });
+  };
 
   return (
     <div>
       <h1>Sign Up</h1>
       <div>
-        {message.map((v, i) => (<p key={i}>{v}</p>))}
+        {message.map((v) => (<p key={v}>{v}</p>))}
       </div>
       <form>
         <div>
-          <label>Name</label>
           <input
-            type='text'
-            name='name'
+            type="text"
+            name="name"
             value={state.name}
             onChange={handleInput}
+            placeholder="Name"
             required
           />
         </div>
 
         <div>
-          <label>Email</label>
           <input
-            type='email'
-            name='email'
+            type="email"
+            name="email"
             value={state.email}
             onChange={handleInput}
+            placeholder="Email"
             required
           />
         </div>
 
         <div>
-          <label>Password</label>
           <input
-            type='password'
-            name='password'
+            type="password"
+            name="password"
             value={state.password}
             onChange={handleInput}
-            required minLength={5} maxLength={50}
+            placeholder="Password"
+            required
+            minLength={5}
+            maxLength={50}
           />
         </div>
 
         <div>
-          <label>Confirmation Password</label>
           <input
-            type='confirmation_password'
-            name='confirmation_password'
+            placeholder="Confirmation Password"
+            type="password"
+            name="confirmation_password"
             value={state.confirmation_password}
             onChange={handleInput}
-            required minLength={5} maxLength={50}
+            required
+            minLength={5}
+            maxLength={50}
           />
         </div>
 
@@ -90,12 +94,14 @@ const Registration = () => {
           <button
             type="submit"
             onClick={handleSubmit}
-          >Submit</button>
+          >
+            Submit
+          </button>
         </div>
 
       </form>
     </div>
-  )
+  );
 };
 
 export default Registration;
