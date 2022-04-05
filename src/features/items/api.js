d36c76974c226800bc95ef452966a2250614e6e1
Moises <mhdezcoronado@gmail.com>
2022-04-05 12:46:56 +0200

    Create createItem method to post data to api

--- src/features/items/api.js
+++ src/features/items/api.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://comic-dance-club.herokuapp.com/api/dances';
+const BASE_URL = 'http://localhost:3000/api/dances';
 
 export const fetchItems = async () => {
   const response = await axios.get(BASE_URL);
   return response.data;
 };
 
-export const createItems = async () => {
-  const response = await axios.get(BASE_URL);
-  return response.data;
+export const createItem = async (payload = {}) => {
+  const token = JSON.parse(localStorage.getItem('token'));
+  const res = await axios.post(BASE_URL, { item: payload }, {
+    headers: { Authorization: token },
+  });
+  return res.data;
 };
