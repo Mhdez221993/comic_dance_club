8716390582a9de81be45c8c4f133f7e0293147fb
Moises <mhdezcoronado@gmail.com>
2022-04-05 18:50:19 +0200

    Create Reservation component

--- src/features/reservation/Index.js
+++ src/features/reservation/Index.js
@@ -1,3 +1,30 @@
-const Reservations = () => <h1>Reservations page</h1>;
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadReservations } from './reducer';
+
+const Reservations = () => {
+  const dispatch = useDispatch();
+  const reservations = useSelector((state) => state.reservationsReducer.reservations);
+  useEffect(() => {
+    dispatch(loadReservations());
+  }, []);
+
+  return (
+    <div>
+      <h1>My reservations</h1>
+      <ul>
+        {
+          reservations.map((v) => (
+            <li key={v.id}>
+              <p>{v.date}</p>
+              <p>{v.city}</p>
+              <p>{v.item.name}</p>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
 
 export default Reservations;
