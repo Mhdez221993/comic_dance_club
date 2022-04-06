d1d2188a941a84f9be65ab338d991a92ae1d4316
Moises <mhdezcoronado@gmail.com>
2022-04-06 12:13:01 +0200

    Add link to create new items if role == admin

--- src/features/nav/Index.js
+++ src/features/nav/Index.js
@@ -1,27 +1,32 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
 import './index.css';
 
-const NavBar = ({ title, routes }) => (
-  <nav className="navBar">
-    <ul className="nav-list">
-      <li className="brand">
-        <Link to="/" className="nav-brand">
-          <h1 className="nav-title">{title}</h1>
-        </Link>
-      </li>
-      {routes.map(
-        ({ name, path }) => name !== 'Ditails' && name !== 'Delete'
-          && (
-            <li key={path} className="nav-links">
-              <NavLink className="links" exact="true" to={path}>{name}</NavLink>
-            </li>
-          ),
-      )}
-    </ul>
-  </nav>
-);
+const NavBar = ({ title, routes }) => {
+  const role = useSelector((state) => state.authReducer.role);
+
+  return (
+    <nav className="navBar">
+      <ul className="nav-list">
+        <li className="brand">
+          <Link to="/" className="nav-brand">
+            <h1 className="nav-title">{title}</h1>
+          </Link>
+        </li>
+        {routes.map(
+          ({ name, path }) => name !== 'Ditails' && name !== 'Delete' && ((!role || role === 'default') && name !== 'Create New Item')
+            && (
+              <li key={path} className="nav-links">
+                <NavLink className="links" exact="true" to={path}>{name}</NavLink>
+              </li>
+            ),
+        )}
+      </ul>
+    </nav>
+  );
+};
 
 NavBar.propTypes = {
   title: PropTypes.string.isRequired,
