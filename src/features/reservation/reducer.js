5a326f2949a59d2c047ec5b24819ce4bc1ee4aa2
Moises <mhdezcoronado@gmail.com>
2022-04-06 11:16:35 +0200

    Create createReservation action and reducer

--- src/features/reservation/reducer.js
+++ src/features/reservation/reducer.js
@@ -2,10 +2,17 @@
 import * as API from './api';
 
 const SET_LOADITEMS = 'reservation/loadItems/SET_LOADITEMS';
+const SET_CREATE_RESERVATION = 'reservation/new/SET_CREATE_RESERVATION';
 
 const initialState = {
   reservations: [],
+  confirmation: '',
 };
+
+const setcreateReservation = (payload) => ({
+  type: SET_CREATE_RESERVATION,
+  payload,
+});
 
 const setLoadReservations = (payload) => ({
   type: SET_LOADITEMS,
@@ -19,10 +26,18 @@ export const loadReservations = () => async (dispatch) => {
   }
 };
 
+export const createReservation = (payload) => async (dispatch) => {
+  const data = await API.createReservation(payload);
+  dispatch(setcreateReservation(data));
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_LOADITEMS:
       return { ...state, reservations: action.payload };
+
+    case SET_CREATE_RESERVATION:
+      return { ...state, confirmation: action.payload };
 
     default:
       return state;
